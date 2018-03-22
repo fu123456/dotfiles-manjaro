c69f4d34b892ffbf523b7ae261d0b82ae742d5fe
fu123456 <xyzgfu@gmail.com>
2018-03-23 03:03:39 +0800

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -80,24 +80,24 @@ echo "Finish installing vim"
 ###################spacemacs##############################
 ##########################################################
 
-# # Install spacemacs
-# today=`date +%Y%m%d`
-# if [ ! -d $HOME/.emacs.d ];then
-    # mkdir -p $HOME/.emacs.d
-# else
-    # mv $HOME/.emacs.d $HOME/.emacs.d/$today 
-    # mkdir -p $HOME/.emacs.d
-# fi
-# echo "clone spacemacs"
-# git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-# echo "Backing up current emacs config"
-# for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private; 
-# do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today;
-# done
-# for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i ; done
-# ln -sfn ${dir}/.emacs.d/init.el ${HOME}/.emacs.d/init.el
-# ln -sfn ${dir}/.spacemacs ${HOME}/.spacemacs
-# ln -sfn ${dir}/.emacs.d/private ${HOME}.emacs.d/private
-# echo "Please start your emacs to install spacemacs plugins ..."
+# Install spacemacs
+today=`date +%Y-%m-%d-%H-%M`
+if [ ! -d $HOME/.emacs.d ];then
+    mkdir -p $HOME/.emacs.d
+else
+    mv $HOME/.emacs.d $HOME/.emacs.d/$today 
+    mkdir -p $HOME/.emacs.d
+fi
+echo "clone spacemacs"
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+echo "Backing up current emacs config"
+for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private; 
+do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today;
+done
+for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private; do [ -L $i ] && unlink $i ; done
+ln -sfn ${dir}/.emacs.d/init.el ${HOME}/.emacs.d/init.el
+ln -sfn ${dir}/.spacemacs ${HOME}/.spacemacs
+ln -sfn ${dir}/.emacs.d/private ${HOME}.emacs.d/private
+echo "Please start your emacs to install spacemacs plugins ..."
 
 ###########################################################
