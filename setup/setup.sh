ffd9200b629357d60cc5de2225cc8cb01ba59b7c
fu123456 <xyzgfu@gmail.com>
2018-03-23 03:33:33 +0800

    update config

--- setup/setup.sh
+++ setup/setup.sh
@@ -81,18 +81,26 @@ echo "Finish installing vim"
 ##########################################################
 
 # Install spacemacs
+# Install auctex
+cd ~
+wget -c http://ftp.gnu.org/pub/gnu/auctex/auctex-12.1.tar.gz
+tar -xzvf auctex-12.1.tar.gz
+cd auctex-12.1
+./configure --prefix=$HOME/.emacs.d/site-lisp/auctex --with-lispdir=$HOME/.emacs.d/site-lisp/auctex --with-texmf-dir=$HOME/.local/lib/texmf &&
+make &&
+sudo make install
 today=`date +%Y-%m-%d-%H-%M`
 if [ ! -d $HOME/.emacs.d ];then
     mkdir -p $HOME/.emacs.d
 else
     cd ~
-    mv .emacs.d .emacs.d-${today} 
+    mv .emacs.d .emacs.d-${today}
     mkdir -p $HOME/.emacs.d
 fi
 echo "clone spacemacs"
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 echo "Backing up current emacs config"
-for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private; 
+for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private;
 do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today;
 done
 for i in $HOME/.spacemacs $HOME/.emacs.d/init.el $HOME/.emacs.d/private; do [ -L $i ] && unlink $i ; done
